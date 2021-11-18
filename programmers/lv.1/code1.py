138371799d593935aa3ef9ce077b5401f77d5dbe
kimtothechang <88170881+kimtothechang@users.noreply.github.com>
2021-11-18 21:09:54 +0900

    UPDATE study

--- programmers/lv.1/code1.py
+++ programmers/lv.1/code1.py
@@ -28,9 +28,13 @@ def solution(lottos, win_nums):
     
     return answer
 
-lottos_list = [[44,1,0,0,31,25],[1,0,0,0,0,0],[45,4,35,20,3,9]]
-winner_list = [[31,10,45,1,6,19],[38,19,20,40,15,25],[20,9,3,45,4,35]]
+# def solution(lottos, win_nums):
 
+#     rank=[6,6,5,4,3,2,1]
 
-for i in range(3):
-    print("answer:", solution(lottos_list[i],winner_list[i]))
+#     cnt_0 = lottos.count(0)
+#     ans = 0
+#     for x in win_nums:
+#         if x in lottos:
+#             ans += 1
+#     return rank[cnt_0 + ans],rank[ans]
---
+++ programmers/lv.1/code2.py
@@ -0,0 +1,55 @@
+def solution(s):
+    dic1 = {
+        0:'zero',
+        1:'one',
+        2:'two',
+        3:'three',
+        4:'four',
+        5:'five',
+        6:'six',
+        7:'seven',
+        8:'eight',
+        9:'nine'
+    }
+    dic2 = ['0','1','2','3','4','5','6','7','8','9']
+    
+    answer = ''
+    char_num = ''
+    count = 0
+
+    
+    for char in s:
+        if char.isalpha():
+            print(char)
+            char_num += char
+            count += 1
+            print(count)
+            for i in range(10):
+                if char_num == dic1[i]:
+                    answer += dic2[i]
+                    count = 0
+                    char_num = ''
+        else:
+            answer += char
+
+    
+    return int(answer)
+
+# num_dic = {
+#     "zero":"0",
+#     "one":"1",
+#     "two":"2",
+#     "three":"3",
+#     "four":"4",
+#     "five":"5",
+#     "six":"6",
+#     "seven":"7",
+#     "eight":"8",
+#     "nine":"9"
+#     }
+
+# def solution(s):
+#     answer = s
+#     for key, value in num_dic.items():
+#         answer = answer.replace(key, value)
+#     return int(answer)
