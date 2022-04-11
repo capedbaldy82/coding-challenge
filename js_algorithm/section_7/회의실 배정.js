cad738c121c536aa612d3b4d3c9550256164b45b
chango <kimtothechang@gmail.com>
2022-04-12 05:38:38 +0900

    :memo: Solve a question

---
+++ js_algorithm/section_7/회의실 배정.js
@@ -0,0 +1,32 @@
+function solution(meeting) {
+  let answer = 0;
+
+  for (let i = 0; i < meeting.length; i++) {
+    let idx = meeting[i][1];
+    let temp = 1;
+    for (let j = i; j < meeting.length; j++) {
+      if (idx <= meeting[j][0]) {
+        temp++;
+        idx = meeting[j][1];
+      }
+    }
+    if (answer < temp) answer = temp;
+  }
+
+  return answer;
+}
+
+let arr1 = [
+  [1, 4],
+  [2, 3],
+  [3, 5],
+  [4, 6],
+  [5, 7],
+];
+
+let arr2 = [
+  [3, 3],
+  [1, 3],
+  [2, 3],
+];
+console.log(solution(arr1));
